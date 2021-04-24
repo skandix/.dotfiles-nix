99086dd4ceb93e3ba87aea70cf9dd1e5c5baae5b
Bendik Lambda <bendik.dyrli@gmail.com>
2021-04-24 20:32:31 +0200

    set static sampling rate, so programs don't get to fuck with that
    Signed-off-by: Bendik Lambda <bendik.dyrli@gmail.com>

--- common/pulseaudio.nix
+++ common/pulseaudio.nix
@@ -12,6 +12,22 @@
       load-module module-switch-on-connect
       unload-module module-switch-on-port-available
     '';
+    daemon.config = { 
+      default-sample-format = "float32le";
+      default-sample-rate = "48000";
+      default-sample-channels = "2";
+      default-channel-map = "front-left,front-right";
+      default-fragments = "2";
+      default-fragment-size-msec = "125";
+      resample-method = "soxr-vhq";
+      enable-lfe-remixing = "no";
+      high-priority = "yes";
+      nice-level = "-11";
+      realtime-scheduling = "yes";
+      realtime-priority = "9";
+      rlimit-rtprio = "9";
+      daemonize = "no";
+    };
   };
   home-manager.users.skandix = {
     home.packages = with pkgs; [
