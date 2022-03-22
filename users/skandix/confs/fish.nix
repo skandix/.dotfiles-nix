5a221392d21f0db61c93b693cbe412f9b40eaa07
Bendik LAmbda <bendik@datapor.no>
2022-03-22 12:57:45 +0100

    yeet
    Signed-off-by: Bendik LAmbda <bendik@datapor.no>

--- users/skandix/confs/fish.nix
+++ users/skandix/confs/fish.nix
@@ -3,7 +3,6 @@
 {
   programs.fish = {
     enable = true;
-    package = pkgs.unstable.fish;
     shellAliases = {
       "sprunge" = "curl -F 'sprunge=<-' http://sprunge.us";
       ".." = "cd ..";
