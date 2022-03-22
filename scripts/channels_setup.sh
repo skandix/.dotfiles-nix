443a500ee4cd449be14e6a9ecca4cbe379553fe2
Bendik LAmbda <bendik@datapor.no>
2022-03-22 16:22:16 +0100

    patch meeh
    Signed-off-by: Bendik LAmbda <bendik@datapor.no>

--- scripts/channels_setup.sh
+++ scripts/channels_setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-nix-channel --add https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz nixos
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-
+nix-channel --add https://channels.nixos.org/nixos-21.11 nixos
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
 nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
-nix-channel --add https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-unstable.tar.gz unstable
+nix-channel --add https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz unstable
 nix-channel --add https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-unstable-small.tar.gz unstable-small
 
 nix-channel --update
