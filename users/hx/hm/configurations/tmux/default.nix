0d3efe928370e7df074463dda684b215b5970c79
Bendik Dyrli <skandix@protonmail.com>
2024-01-14 02:28:51 +0100

    try2
    Signed-off-by: Bendik Dyrli <skandix@protonmail.com>

--- users/hx/hm/configurations/tmux/default.nix
+++ users/hx/hm/configurations/tmux/default.nix
@@ -1,12 +1,14 @@
-{pkgs, ...}:
+{pkgs, home-manager, ...}:
 
 {
-    home.packages = with pkgs; [
-        tmux
-        tmux-cssh
-    ];
-    home.file.tmux = {
-        source = ./files/tmux.conf;
-        target = ".tmux.conf";
+    home-manager.users.hx = {
+        home.packages = with pkgs; [
+            tmux
+            tmux-cssh
+        ];
+        home.file.tmux = {
+            source = ./files/tmux.conf;
+            target = ".tmux.conf";
+        };
     };
 }
