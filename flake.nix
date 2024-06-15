c7e1054c3a224ca92cb4106762806a1331ea83ff
Bendik Dyrli <skandix@protonmail.com>
2024-06-15 23:57:44 +0200

    jiafwe f
    Signed-off-by: Bendik Dyrli <skandix@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -12,21 +12,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nur.url = "github:nix-community/NUR";
+    # nur.url = "github:nix-community/NUR";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    nix-gaming.url = "github:fufexan/nix-gaming";
+    # darwin = {
+    #   url = "github:LnL7/nix-darwin";
+    #   inputs.nixpkgs.follows = "nixpkgs-unstable";
+    # };
+    # nix-gaming.url = "github:fufexan/nix-gaming";
   };
 
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nur, nixos-hardware, home-manager, nix-gaming, darwin, ...}:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager, ...}:
     let
       system = "x86_64-linux";
       # pkgs = nixpkgs.legacyPackages."x86_64-linux";
