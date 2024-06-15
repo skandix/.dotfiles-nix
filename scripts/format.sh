00fc377f5a0e6a9347e527aca866cf8360f32b16
hx <skandix@datapor.no>
2024-06-15 17:34:50 +0200

    update format script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/env bash
+
+echo " ███▄    █  ██▓▒██   ██▒ ▒█████    ██████ "
+echo " ██ ▀█   █ ▓██▒▒▒ █ █ ▒░▒██▒  ██▒▒██    ▒ "
+echo "▓██  ▀█ ██▒▒██▒░░  █   ░▒██░  ██▒░ ▓██▄   "
+echo "▓██▒  ▐▌██▒░██░ ░ █ █ ▒ ▒██   ██░  ▒   ██▒"
+echo "▒██░   ▓██░░██░▒██▒ ▒██▒░ ████▓▒░▒██████▒▒"
+echo "░ ▒░   ▒ ▒ ░▓  ▒▒ ░ ░▓ ░░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░"
+echo "░ ░░   ░ ▒░ ▒ ░░░   ░▒ ░  ░ ▒ ▒░ ░ ░▒  ░ ░"
+echo "   ░   ░ ░  ▒ ░ ░    ░  ░ ░ ░ ▒  ░  ░  ░  "
+echo "         ░  ░   ░    ░      ░ ░        ░  "
+echo "                                          "
+
 disk=$1
-echo "PARTITION"
+echo ""
+echo "===P A R T I T I O N==="
 echo ""
 parted $disk -- mklabel gpt
-parted $disk -- mkpart primary 512MiB -200MiB
-parted $disk -- mkpart primary linux-swap -200MiB 100%
+parted $disk -- mkpart root ext4 512MiB -200MiB
+parted $disk -- mkpart swap linux-swap -200MiB 100%
 parted $disk -- mkpart ESP fat32 1MiB 512MiB
 parted $disk -- set 3 esp on
 
-echo "FORMATING"
 echo ""
-mkfs.ext4 -L nixos "$disk"1
-mkswap -L swap "$disk"2
-mkfs.fat -F 32 -n boot "$disk"3
+echo "===F O R M A T I N G==="
+echo ""
+mkfs.ext4 -L nixos $disk"p1"
+mkswap -L swap $disk"p2"
+mkfs.fat -F 32 -n boot $disk"p3"
 
-echo "MOUNTING"
 echo ""
-mount /dev/disk/by-label/nixos /mnt
-mkdir -p /mnt/boot
-mount /dev/disk/by-label/boot /mnt/boot
+echo "===M O U N T I N G==="
+echo ""
+mount /dev/disk/by-label/nixos /mnt --mkdir
+mount /dev/disk/by-label/boot /mnt/boot --mkdir
 swapon /dev/disk/by-label/swap
