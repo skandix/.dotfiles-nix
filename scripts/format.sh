95fb2f06397ca1071b89c577f547fc95c268cf8c
hx <skandix@datapor.no>
2024-06-15 18:18:36 +0200

    sdfsdfa

--- scripts/format.sh
+++ scripts/format.sh
@@ -15,11 +15,11 @@ disk=$1
 echo ""
 echo "===P A R T I T I O N==="
 echo ""
-parted $disk -- mklabel gpt
-parted $disk -- mkpart root ext4 512MiB -8GB
-parted $disk -- mkpart swap linux-swap -8GB 100%
-parted $disk -- mkpart ESP fat32 1MiB 512MiB
-parted $disk -- set 3 esp on
+parted -s $disk -- mklabel gpt
+parted -s $disk -- mkpart root ext4 512MiB -8GB
+parted -s $disk -- mkpart swap linux-swap -8GB 100%
+parted -s $disk -- mkpart ESP fat32 1MiB 512MiB
+parted -s $disk -- set 3 esp on
 
 echo ""
 echo "===F O R M A T I N G==="
@@ -31,6 +31,10 @@ mkfs.fat -F 32 -n boot $disk"p3"
 echo ""
 echo "===M O U N T I N G==="
 echo ""
+umount -a
+mount -v
 mount /dev/disk/by-label/nixos /mnt --mkdir
+mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot --mkdir
 swapon /dev/disk/by-label/swap
+mount -v
