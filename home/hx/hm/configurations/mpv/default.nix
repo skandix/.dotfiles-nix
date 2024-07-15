6c09e59e716ce7c76d91e2153d96bf41d7e02905
Bendik Dyrli <skandix@protonmail.com>
2024-07-15 12:37:39 +0200

    back to work
    Signed-off-by: Bendik Dyrli <skandix@protonmail.com>

--- home/hx/hm/configurations/mpv/default.nix
+++ home/hx/hm/configurations/mpv/default.nix
@@ -2,7 +2,7 @@
 
 {
   home-manager.users.hx = {
-    home.packages = with pkgs; [ mpv youtube-dl streamlink ];
+    home.packages = with pkgs; [ mpv yt-dlp streamlink ];
 
     xdg.configFile = { "mpv/mpv.conf".source = ./mpv.conf; };
   };
