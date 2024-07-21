7eaee6eb0517cc21021b32dd735eff89dba3c4c6
Bendik Dyrli <skandix@datapor.no>
2024-07-21 13:40:16 +0200

    rofi themes
    Signed-off-by: Bendik Dyrli <skandix@datapor.no>

--- home/hx/hm/configurations/rofi/default.nix
+++ home/hx/hm/configurations/rofi/default.nix
@@ -4,5 +4,6 @@
   home-manager.users.hx = {
     home.packages = with pkgs; [ rofi ];
     xdg.configFile = { "rofi/config.rasi".source = ./config.rasi; };
+    xdg.configFile = { "rofi/spotlight.rasi".source = ./spotlight.rasi; };
   };
 }
