9c304e752dbba35f0fd71d2ae7b0fd39b6b66c7f
Bendik Dyrli <skandix@datapor.no>
2024-07-22 22:26:43 +0200

    wallpapers and more
    Signed-off-by: Bendik Dyrli <skandix@datapor.no>

--- home/hx/hm/python.nix
+++ home/hx/hm/python.nix
@@ -5,7 +5,7 @@
     home.packages = with pkgs;
     [
       (python312.withPackages
-        (ps: with ps; [ neovim requests setuptools pip pipenv loguru pipenv ]))
+        (ps: with ps; [ neovim requests setuptools pip pipenv loguru pipenv pandas tabulate ]))
       ];
   };
 }
