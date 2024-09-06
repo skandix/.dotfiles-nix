34c95510ace48ef41dac1b5aa786c9c9fe0b4e0c
Bendik Dyrli <skandix@protonmail.com>
2024-09-06 23:09:31 +0200

    more stuff
    Signed-off-by: Bendik Dyrli <skandix@protonmail.com>

--- hosts/SpaceCruiser/modules/system.nix
+++ hosts/SpaceCruiser/modules/system.nix
@@ -13,6 +13,31 @@
 
     defaults = {
       menuExtraClock.Show24Hour = true;  # show 24 hour clock
+
+      dock = {
+        autohide = true; # automatically hide and show 
+        show-recents = false; # do not show recent apps in dock
+        # do not automatically rearrange spaces based on most recent use.
+        mru-spaces = false;
+        expose-group-by-app = true; # Group windows by application
+    };
+    NSGlobalDomain = {
+        # `defaults read NSGlobalDomain "xxx"`
+        "com.apple.swipescrolldirection" = false; # enable natural scrolling(default to true)
+        "com.apple.sound.beep.feedback" = 0; # disable beep sound when pressing volume up/down key
+
+        # Appearance
+        AppleInterfaceStyle = "Dark"; # dark mode
+
+        AppleKeyboardUIMode = 3; # Mode 3 enables full keyboard control.
+        ApplePressAndHoldEnabled = true; # enable press and hold
+
+        # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
+        # This is very useful for vim users, they use `hjkl` to move cursor.
+        # sets how long it takes before it starts repeating.
+        InitialKeyRepeat = 15; # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+        # sets how fast it repeats once it starts.
+        KeyRepeat = 3; # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
     };
   };
 
