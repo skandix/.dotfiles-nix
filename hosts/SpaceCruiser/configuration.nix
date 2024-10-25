11f825da304d8b8cd97e2789d2466ec192e3d4d2
Bendik Dyrli <skandix@protonmail.com>
2024-10-25 15:37:36 +0200

    stuff
    Signed-off-by: Bendik Dyrli <skandix@protonmail.com>

---
+++ hosts/SpaceCruiser/configuration.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Hardware udev rules
+    ./hardware-configuration.nix
+
+    # core dotfiles + graphical things
+    ../../home
+    ../../home/hx
+    ../../home/hx/gui.nix
+    ../../home/hx/cli.nix
+    ../../home/hx/i3
+
+    # Common
+    ../../common/intelcpu.nix
+    ../../common/fingerprint.nix
+    ../../common/laptop.nix
+    ../../common/intelgpu.nix
+    ../../common/docker.nix
+    ../../common/fonts.nix
+    ../../common/networkmanager.nix
+    ../../common/pipewire.nix
+    ../../common/networking-extra.nix
+    ../../common/security.nix
+    ../../common/nix-pkg-allow.nix
+    ../../common/fwupd.nix
+    ../../common/zsh.nix
+    ../../common/ssh-client.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  systemd.services.NetworkManager-wait-online.enable = false; # to avoid iface or vbox waiting for connection.
+  networking.hostName = "SpaceCruiser";
+  networking.useDHCP = false;
+  networking.interfaces.enp5s0.useDHCP = true;
+  networking.interfaces.wlp6s0.useDHCP = false;
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console = { keyMap = "no"; };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  time.timeZone = "Europe/Oslo";
+  home-manager.users.hx.home.stateVersion = "24.05";
+  system.stateVersion = "24.05";
+  }
