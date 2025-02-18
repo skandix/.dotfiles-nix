807cca95b6bf3f96d967042ef8c261b1fe683658
Bendik Dyrli <skandix@datapor.no>
2025-02-18 11:05:20 +0100

    netowkring
    Signed-off-by: Bendik Dyrli <skandix@datapor.no>

--- common/networkmanager.nix
+++ common/networkmanager.nix
@@ -1,6 +1,21 @@
 { config, pkgs, ... }:
 
 {
+  #networking.nameservers = [
+    #"8.8.8.8"
+    #"8.8.4.4"
+  #];
+  networking.enableIPv6 = true;
+  networking.wireguard = {
+    enable = false;
+  };
+
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "both";
+    openFirewall = true;
+    interfaceName = "ts0";
+  };
   networking.networkmanager.enable = true;
 
   environment.systemPackages = with pkgs; [
