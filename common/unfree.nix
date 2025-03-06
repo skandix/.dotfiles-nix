1cc3dcfcf91d96b5d063d5f9e20055a311d3ca1c
Bendik Dyrli <skandix@datapor.no>
2025-03-06 21:51:33 +0100

    added seperate file for unfree software :(
    Signed-off-by: Bendik Dyrli <skandix@datapor.no>

--- common/unfree.nix
+++ common/unfree.nix
@@ -2,11 +2,13 @@
   config,
   pkgs,
   lib,
+  unstable,
   ...
 }:
 
 {
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkgs) [
     "winbox4"
   ];
+  environment.systemPackages = with pkgs; [ winbox4 ];
 }
