8ee1afcea2bddb6792d4b6f63e96c79eb3e62217
skandix <skandix@thevoyager.hen-gorgon.ts.net>
2025-03-11 12:06:12 +0100

    removed subs
    Signed-off-by: skandix <skandix@thevoyager.hen-gorgon.ts.net>

--- hosts/TheVoyager/modules/nix-core.nix
+++ hosts/TheVoyager/modules/nix-core.nix
@@ -18,13 +18,13 @@
     # auto-optimise-store = true;
 
     # substituers that will be considered before the official ones(https://cache.nixos.org)
-    substituters = [
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    # substituters = [
+    #   "https://mirror.sjtu.edu.cn/nix-channels/store"
+    #   "https://nix-community.cachix.org"
+    # ];
+    # trusted-public-keys = [
+    #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    # ];
     builders-use-substitutes = true;
   };
 
