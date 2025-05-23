88996f65d6e7b062ce63efdf541a9688cc60547b
Bendik Dyrli <skandix@datapor.no>
2025-05-23 02:48:08 +0200

    sieofj
    Signed-off-by: Bendik Dyrli <skandix@datapor.no>

--- home/hx/hm/configurations/thunderbird/default.nix
+++ home/hx/hm/configurations/thunderbird/default.nix
@@ -6,9 +6,6 @@
       programs.thunderbird = {
           enable = true;
           package = unstable.thunderbird;
-          settings = {
-              "privacy.donottrackheader.enabled" = true;
-          };
       };
   };
 }
