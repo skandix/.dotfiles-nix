c6732aede10b0ae0e709928906fd56a1c9c74e15
hx <hx@datapor.no>
2025-12-14 22:47:11 +0100

    sfeow q
    Signed-off-by: hx <hx@datapor.no>

--- common/exporters.nix
+++ common/exporters.nix
@@ -1,10 +1,16 @@
 { config, pkgs, ... }:
 
 {
-
     services.prometheus.exporters = {
         zfs = {
             enable = true;
+            openFirewall = true;
+        };
+
+        node = {
+            enable = true;
+            openFirewall = true;
+            port = 9101;
         };
     };
 }
