8166893b8ed39f784ea6c5b24b13960c0241481e
hx <hx@datapor.no>
2025-12-23 14:44:18 +0100

    fixing things
    Signed-off-by: hx <hx@datapor.no>

---
+++ hosts/MillenniumFalcon/disk-config.nix
@@ -0,0 +1,43 @@
+{lib, ...}:
+
+{
+  disko.devices = {
+    disk.main = {
+        device = lib.mkDefault "/dev/vda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+
+            esp = {
+              name ="ESP";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+
+          };
+        };
+    };
+  };
+}
