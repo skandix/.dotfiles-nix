a830a87adf76a7ad26d4dd5d635806477002026b
hx <hx@datapor.no>
2025-12-24 03:18:10 +0100

    perf
    Signed-off-by: hx <hx@datapor.no>

--- common/taskchampion.nix
+++ common/taskchampion.nix
@@ -3,11 +3,10 @@
 {
     services.taskchampion-sync-server = {
         enable = true;
-        user =  "hx";
         snapshot = {
             days = 7;
         };
-        openfirewall = true;
+        openFirewall = true;
         host = "127.0.0.1";
         dataDir = "/home/hx/taskChampion";
     };
