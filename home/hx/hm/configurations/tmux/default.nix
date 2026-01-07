86766d9156f002638552f0334b4e20315c32c46d
hx <hx@datapor.no>
2026-01-07 23:36:09 +0100

    somethings needs to die
    Signed-off-by: hx <hx@datapor.no>

--- home/hx/hm/configurations/tmux/default.nix
+++ home/hx/hm/configurations/tmux/default.nix
@@ -11,20 +11,20 @@
       clock24 = true;
       plugins  = with pkgs; [
         tmuxPlugins.cpu
-        {
-          plugin = tmuxPlugins.resurrect;
-          extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-        }
+        #{
+          #plugin = tmuxPlugins.resurrect;
+          #extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+        #}
         {
           plugin = tmuxPlugins.catppuccin;
         }
-        {
-          plugin = tmuxPlugins.continuum;
-          extraConfig = ''
-            set -g @continuum-restore 'on'
-            set -g @continuum-save-interval '60' # minutes
-          '';
-        }
+        #{
+          #plugin = tmuxPlugins.continuum;
+          #extraConfig = ''
+            #set -g @continuum-restore 'on'
+            #set -g @continuum-save-interval '60' # minutes
+          #'';
+        #}
       ];
       extraConfig = ''
         set -sg escape-time 0
