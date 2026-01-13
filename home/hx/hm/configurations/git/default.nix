608191498ffe2e749727ee3fa9c235e5f62d5324
hx <hx@datapor.no>
2026-01-13 01:41:01 +0100

    nix lazygit
    added lazygit through nix derivations

--- home/hx/hm/configurations/git/default.nix
+++ home/hx/hm/configurations/git/default.nix
@@ -1,27 +1,36 @@
-{ pkgs, ... }:
+{ pkgs, unstable, ... }:
 
 {
-  programs.git = {
-    package = pkgs.gitFull;
-    enable = true;
-    settings = {
-      core.editor = "vim";
-      init.defaultBranch = "main";
-      pull.rebase = true;
-      push.autoSetupRemote = true;
-      rebase.autosquash = true;
-      commit.verbose = true;
-      diff.algorithm = "histogram";
-      url."ssh://git@github.com:".insteadOf = "git://github.com";
-      rerere.enabled = true;
-      # identities
-      user = {
-        name = "hx";
-        email = "hx@datapor.no";
-        useConfigOnly = true;
+  programs = {
+    git = {
+      package = pkgs.gitFull;
+      enable = true;
+      settings = {
+        core.editor = "vim";
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        push.autoSetupRemote = true;
+        rebase.autosquash = true;
+        commit.verbose = true;
+        diff.algorithm = "histogram";
+        url."ssh://git@github.com:".insteadOf = "git://github.com";
+        rerere.enabled = true;
+        # identities
+        user = {
+          name = "hx";
+          email = "hx@datapor.no";
+          useConfigOnly = true;
+        };
       };
     };
-  };
 
-  home.packages = with pkgs; [ lazygit ];
+    lazygit = {
+      enable = true;
+      package = unstable.lazygit;
+      #settings = {
+        #nerdFontsVersion="3";
+      #};
+    };
+};
+
 }
