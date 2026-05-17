0cddae85d46dd252b3e4d6bad3c9eb3fd7a8e153
Bendik Dyrli <skandix@datapor.no>
2026-05-18 01:00:10 +0200

    lots of new wayland thingies

--- home/hx/hm/configurations/satty/default.nix
+++ home/hx/hm/configurations/satty/default.nix
@@ -4,11 +4,12 @@
   home-manager.users.hx = {
     programs.satty = {
       enable = true;
-      settings = {
-        general = {
-          output-filename = "/home/hx/Pictures/%Y-%m-%d_%H:%M:%S.png"
-        }
-      };
+      # settings = {
+      #   general = {
+      #     output-filename = "/home/hx/Pictures/%Y-%m-%d_%H:%M:%S.png"
+      #   }
+      # };
+      # TODO: something is not working as it should, so i'll look into this late, just need to get the rest working
     };
   };
 }
--- home/hx/mango/default.nix
+++ home/hx/mango/default.nix
@@ -4,13 +4,15 @@
   imports = [
     inputs.mangowm.nixosModules.mango
 
-    ./../hm/configurations/flameshot
+    ./../hm/configurations/satty
+    ./../hm/configurations/waybar
     ./../hm/configurations/rofi
     ./../hm/configurations/wpaperd
     ./../hm/configurations/librewolf
     ./../hm/configurations/ghostty
     ./../hm/configurations/mpv
     ./../hm/configurations/discord
+    ./../hm/configurations/udiskie
     ./../hm/configurations/vim
     ./../hm/configurations/tmux
     ./../hm/go.nix
@@ -25,10 +27,10 @@
     services.greetd = {
       enable = true;
       settings = {
-        initial_session = {
-          command = "mango";
-          user = "hx"; # auto-login on first start, no password required
-        };
+        # initial_session = {
+        #   command = "mango";
+        #   user = "hx"; # auto-login on first start, no password required
+        # };
         default_session = {
           command = "${pkgs.tuigreet}/bin/tuigreet --cmd mango";
           user = "greeter";
@@ -48,7 +50,16 @@
   home-manager.users.hx = {
     xdg.configFile = {
       "mango/config.conf".source = ./config.conf;
+      "mango/startup.conf".source = ./startup.conf;
+      "mango/keybind.conf".source = ./keybind.conf;
+      "mango/mouse.conf".source = ./mouse.conf;
+      "mango/visual.conf".source = ./visual.conf;
+      "mango/window.conf".source = ./window.conf;
     };
+  };
+
+  environment.variables = {
+    TERMINAL = "xterm-256color"; # so stuff don't shit the bed when they see a ghost
   };
 
 }
