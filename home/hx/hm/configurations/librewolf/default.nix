e272ba16d04e603502995bfe7e801c188314a732
Bendik Dyrli <skandix@datapor.no>
2026-05-31 13:45:35 +0200

    removed unstable librewolf package
    Signed-off-by: Bendik Dyrli <skandix@datapor.no>

--- home/hx/hm/configurations/librewolf/default.nix
+++ home/hx/hm/configurations/librewolf/default.nix
@@ -4,14 +4,22 @@
   home-manager.users.hx = {
     programs.librewolf = {
       enable = true;
-      package = unstable.librewolf;
+      #package = pkgs.librewolf;
       settings = {
         "webgl.disabled" = false;
+
         "privacy.resistFingerprinting" = false;
         "privacy.clearOnShutdown.history" = true;
         "privacy.clearOnShutdown.cookies" = false;
+        "privacy.donottrackheader.enabled" = true;
+
+        "cookiebanners.service.mode.privateBrowsing" = 2;
+        "cookiebanners.service.mode" = 2;
+
         "network.cookie.lifetimePolicy" = 0;
+
         "identity.fxaccounts.enabled" = true;
+
         "browser.sessionstore.resume_from_crash" = false;
       };
     };
