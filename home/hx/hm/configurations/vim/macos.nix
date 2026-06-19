9125b2192e927f8989cc2a629c6167b580dc0fba
Bendik Dyrli <bendik.dyrli@gmail.com>
2026-06-19 08:36:44 +0200

    asdf

--- home/hx/hm/configurations/vim/macos.nix
+++ home/hx/hm/configurations/vim/macos.nix
@@ -3,38 +3,39 @@ let
   general = builtins.readFile ./general.vim;
 in
 {
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-    viAlias = true;
-    vimdiffAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      srcery-vim
-      vim-nix
-      nvim-tree-lua
-      vim-indent-guides
-      nerdcommenter
-      vim-startify
-      vim-json
-      vim-better-whitespace
-      lightline-vim
-      lexima-vim
-      ale
-      vim-gitgutter
-      telescope-nvim
-      ansible-vim
-      vim-polyglot
-      rainbow
-      nvim-lspconfig
-    ];
-    extraLuaConfig = ''
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-      vim.opt.termguicolors = true
-      require("nvim-tree").setup()
-    '';
-    extraConfig = ''
-      ${general}
-    '';
-  };
+    programs.neovim = {
+      enable = true;
+      vimAlias = true;
+      viAlias = true;
+      vimdiffAlias = true;
+      defaultEditor = true;
+      plugins = with pkgs.vimPlugins; [
+        srcery-vim
+        vim-nix
+        nvim-tree-lua
+        vim-indent-guides
+        nerdcommenter
+        vim-startify
+        vim-json
+        vim-better-whitespace
+        lightline-vim
+        lexima-vim
+        ale
+        vim-gitgutter
+        telescope-nvim
+        ansible-vim
+        vim-polyglot
+        rainbow
+        nvim-lspconfig
+      ];
+      initLua = ''
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        vim.opt.termguicolors = true
+        require("nvim-tree").setup()
+      '';
+      extraConfig = ''
+        ${general}
+      '';
+    };
 }
